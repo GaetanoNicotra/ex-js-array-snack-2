9df189dcdd42403089d2fbea23d08ee032ce12ad
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-18 17:36:57 +0200

    [UPD] Snack 3 completato

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// SNACK 1
+
 
 const books = [
     {
@@ -47,25 +47,37 @@ const books = [
     },
 ];
 
+// SNACK 1
+
 // libri con piu di 300 pagine
 const longBooks = books.filter(b => b.pages > 300);
-console.log(longBooks);
+console.log('Libri con piu di 300 pagine', longBooks);
 
 // titolo libri con piu di 300 pagine
 const longBooksTitles = longBooks.map(l => l.title);
-console.log(longBooksTitles);
+console.log('Titolo libri con piu di 300 pagine', longBooksTitles);
 
 
 // SANCK 2
 
 // libri disponibili
 const availableBooks = books.filter(b => b.available === true);
-console.log(availableBooks);
+console.log('Libri disponibili', availableBooks);
 
 // libri scontati
 const discountedBooks = availableBooks.map(a => parseInt(a.price) * 0.8);
-console.log(discountedBooks);
+console.log('Libri scontati', discountedBooks);
 
 // libri con prezzo intero
-const fullPricedBook = discountedBooks.find(d => d.price === );
-console.log(fullPricedBook)
+const fullPricedBook = discountedBooks.find(d => Number.isInteger(d));
+console.log('Libri con prezzo intero', fullPricedBook)
+
+// SNACK 3
+
+// autori dei libri
+const authors = books.map(b => b.author).sort((a, b) => b.age - a.age);
+console.log('Autori', authors)
+
+// verifico se gli autori sono maggiorenni
+const areAuthorsAdults = authors.every(a => a.age > 18);
+console.log('Tutti gli autori sono maggiorenni?', areAuthorsAdults);
